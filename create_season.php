f63b857d0d2574fb45068cf4896126fac067acc2
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-02-06 11:18:12 -0800

    New dummy form for create_season.php

--- create_season.php
+++ create_season.php
@@ -21,36 +21,53 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Create new season</h1>
           <div class="table-responsive">
-            <table class="table table-striped">
-            	<thead>
-                	<tr>
-                		<th>Season Name</th>
-                		<th>Start Date</th>
-                		<th>End Date</th>
-                		<th></th>
-                	</tr>
-            	</thead>
-              	<tbody>
-             		<tr>
-                		<td>Season 1</td>
-                		<td>10/10/2014</td>
-                		<td>6/5/2015</td>
-                		<td><a href="#"><button type="button" class="btn btn-default">Edit</button></a></td>
-               		</tr>
-                	<tr>
-                		<td>Season 2</td>
-                		<td>10/10/2015</td>
-                		<td>6/5/2016</td>
-                		<td><a href="#"><button type="button" class="btn btn-default">Edit</button></a></td>
-               		</tr>
-               		<tr>
-                		<td>Season 3</td>
-                		<td>10/10/2016</td>
-                		<td>6/5/2017</td>
-                		<td><a href="#"><button type="button" class="btn btn-default">Edit</button></a></td>
-               		</tr>
-              </tbody>
-            </table>
+            <form>
+              <div class="form-group">
+                <label for="season_name">Season name</label>
+                <input type="text" class="form-control" id="season_name" placeholder="Ex: Season 5">
+              </div>
+              <div class="form-group">
+                <label for="start_date">Start Date</label>
+                <input type="date" class="form-control" id="start_date">
+              </div>
+              <div class="form-group">
+                <label for="end_date">End Date</label>
+                <input type="date" class="form-control" id="end_date">
+              </div>
+              <div class="form-group">
+                <label for="rank_1_pts">Points earned for #1</label>
+                <input type="number" class="form-control" id="rank_1_pts" placeholder="Ex: 32">
+              </div>
+              <div class="form-group">
+                <label for="rank_2_pts">Points earned for #2</label>
+                <input type="number" class="form-control" id="rank_2_pts" placeholder="Ex: 20">
+              </div>
+              <div class="form-group">
+                <label for="rank_3_pts">Points earned for #3</label>
+                <input type="number" class="form-control" id="rank_3_pts" placeholder="Ex: 12">
+              </div>
+              <div class="form-group">
+                <label for="rank_4_pts">Points earned for #4</label>
+                <input type="number" class="form-control" id="rank_4_pts" placeholder="Ex: 8">
+              </div>
+              <div class="form-group">
+                <label for="rank_5_pts">Points earned for #5</label>
+                <input type="number" class="form-control" id="rank_5_pts" placeholder="Ex: 4">
+              </div>
+              <div class="form-group">
+                <label for="rank_7_pts">Points earned for #7</label>
+                <input type="number" class="form-control" id="rank_7_pts" placeholder="Ex: 2">
+              </div>
+              <div class="form-group">
+                <label for="rank_9_pts">Points earned for #9</label>
+                <input type="number" class="form-control" id="rank_9_pts" placeholder="Ex: 1">
+              </div>
+              <div class="form-group">
+                <label for="rank_13_pts">Points earned for #13</label>
+                <input type="number" class="form-control" id="rank_13_pts" placeholder="Ex: 1">
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
           </div>
         </div>
       </div>
